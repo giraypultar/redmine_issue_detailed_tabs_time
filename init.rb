4765262f7eacb2ae2720ab3e3e7d7830f96634c1
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-03-04 11:40:08 +0200

    Refactored plugin with better namespace and class eval for helper

--- init.rb
+++ init.rb
@@ -1,17 +1,24 @@
 require 'redmine'
-require 'issue_tabs_listener'
-require_dependency 'issue_detailed_tabs_time_issues_helper_patch'
+require_dependency 'issues_helper'
+
+ActionDispatch::Callbacks.to_prepare do
+  Dir[File.dirname(__FILE__) + '/lib/redmine_issue_detailed_tabs_time/patches/*_patch.rb'].each {|file|
+    require_dependency file
+  }
+
+  Dir[File.dirname(__FILE__) + '/lib/redmine_issue_detailed_tabs_time/hooks/*_hook.rb'].each {|file|
+    require_dependency file
+  }
+end
 
 Redmine::Plugin.register :redmine_issue_detailed_tabs_time do
-  
   project_module :issue_tracking do
 	  permission :view_all, { :all => :index }
 	  permission :view_comments, { :all => :index }
 	  permission :view_activity, { :all => :index }
   end
-  
   name 'Redmine Issue Detailed Tabs & Time'
   author 'Mark Kalender'
   description 'This plugin provide breaks down issues comments into tabs, also adds a time log'
-  version '0.1.0'
+  version '0.1.1'
 end
