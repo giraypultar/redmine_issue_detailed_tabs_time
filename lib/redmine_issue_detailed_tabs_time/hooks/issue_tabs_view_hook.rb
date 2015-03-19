5e9071b15cf963993df60ce666b9a33207e8b743
Codrut Constantin Gusoi <codrut.gusoi@zitec.ro>
2015-03-19 12:08:02 +0200

    Rendeing details in a hook

--- lib/redmine_issue_detailed_tabs_time/hooks/issue_tabs_view_hook.rb
+++ lib/redmine_issue_detailed_tabs_time/hooks/issue_tabs_view_hook.rb
@@ -1,8 +1,9 @@
 require 'redmine'
 
 class IssueTabsViewListener < Redmine::Hook::ViewListener
-  # Adds javascript
   def view_layouts_base_html_head(context)
     javascript_include_tag('issue-history-tabs.js', :plugin => :redmine_issue_detailed_tabs_time)
   end
+
+  render_on :new_issue_view_details, partial: 'issues/details'
 end
